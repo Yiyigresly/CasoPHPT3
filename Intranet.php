5fdb585e8c7aa0ac26ae5c88980aaaf193709f0c
Yiyigresly <keorza@gmail.com>
2021-11-07 20:16:01 +0100

    EjercicioPHP

---
+++ Intranet.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> <!---->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página WEB1</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+<?php echo "<h1>Título página con PHP<h1>"; ?> 
+<aside>
+   <ul>
+       <li class="square"><a  href="blog.php"><?php  echo"Ir al blog";?></a></li>
+       <li class="square"><a  href="Intranet.php"><?php echo"Ir a la tienda";?></a></li>
+   </ul>
+</aside>
+<main>
+ <h4>Descripción de la página</h4>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, culpa iure? Minima, aliquid! Eos necessitatibus impedit laboriosam
+         perspiciatis officia recusandae labore minus cum sequi aperiam aliquid placeat velit, aut ut.</p>
+</main>
+</body>
+</html>
---
+++ blog.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> <!---->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página WEB2</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+  <?php echo "<h1>Blog<h1>"; ?> 
+
+   <main>
+    <h4>Descripción del Blog</h4>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, culpa iure? Minima, aliquid! Eos necessitatibus impedit laboriosam
+         perspiciatis officia recusandae labore minus cum sequi aperiam aliquid placeat velit, aut ut.</p>
+   </main>
+   <aside>
+    <ul>
+      <li class="none"><a href="Intranet.php"><?php  echo"Volver a Intranet";?></a></li>
+      <li class="none"><a href="blog.php"><?php echo"Ir a sección de noticias";?></a></li>
+    </ul>
+   </aside>
+</body>
+</html>
